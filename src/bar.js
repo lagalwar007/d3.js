04bc052f49bea0da6dd85817225133f00828bc4c
Karan <lagalwar007@gmail.com>
2018-08-17 20:58:46 +0530

    bar chart

---
+++ src/bar.js
@@ -0,0 +1,35 @@
+import * as d3 from "d3";
+
+d3.select("p").text("hello workd");
+d3.select("#app")
+  .append("p")
+  .style("color", "red")
+  .text("I am new add");
+
+var canvas = d3
+  .select("body")
+  .append("svg")
+  .attr("width", 1500)
+  .attr("height", 1500);
+
+canvas
+  .append("circle")
+  .attr("cx", "250")
+  .attr("cy", "250")
+  .attr("r", "50")
+  .style("fill", "red");
+
+canvas
+  .append("rect")
+  .attr("width", 150)
+  .attr("height", 50)
+  .style("fill", "red");
+
+canvas
+  .append("line")
+  .attr("x1", 100)
+  .attr("x2", 201)
+  .attr("y1", 50)
+  .attr("y2", 240)
+  .style("stroke", "green")
+  .style("stroke-width", 5);
